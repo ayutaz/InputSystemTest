59747c4e1eee09fc59048297f2bad73a9af7cd13
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-02 19:11:48 +0900

    feat: Build後に特定のブランチにwebGL用Deploy処理の実行

---
+++ Assets/_Project/Script/Editor/WebGLDeploy.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace _Project.Editor
+{
+    public class WebGLDeploy : IPostprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+        private string _batPath;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            _batPath = Path.Combine(GetBuildFolderPath(), "Build");
+            RunCommand(_batPath);
+        }
+
+        private static string GetBuildFolderPath()
+        {
+            return Directory.GetParent(Application.dataPath)?.FullName;
+        }
+
+        private static void RunCommand(string buildFolderPath)
+        {
+            var process = new Process();
+            process.StartInfo.WorkingDirectory = buildFolderPath;
+            process.StartInfo.FileName = Path.Combine(buildFolderPath, "Deploy.bat");
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
+#endif
